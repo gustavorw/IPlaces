4ec797a5efc23f17b2d77bfdd98cc7f080c74eed
gustavo-wanderley <gustavorw03@gmail.com>
2020-12-07 12:57:37 -0300

    add controller

---
+++ iphotos/lib/controller/home_controller.dart
@@ -0,0 +1,21 @@
+import 'dart:convert';
+import 'package:iphotos/models/place.dart';
+import 'package:mobx/mobx.dart';
+import 'package:http/http.dart' as http;
+part 'home_controller.g.dart';
+
+class HomeController = HomeControllerBase with _$HomeController;
+
+abstract class HomeControllerBase with Store {
+  @action
+  Future<List<Place>> getPlaces() async {
+    print("oi");
+    var response = await http.get('https://iphotos.herokuapp.com/api/places');
+    String data = response.body;
+    print(data);
+    List dataList = json.decode(data);
+    List<Place> places = dataList.map((e) => Place.fromJson(e)).toList();
+    print(places);
+    return places;
+  }
+}
